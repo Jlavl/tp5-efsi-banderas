f78c8e3070025af82bfbf8cb4c07686160e8841c
Julian <jelaverallauro@gmail.com>
2024-08-07 09:04:57 -0300

    recibo los datos pero no los puedo hacerlos string

--- src/app/components/bandera.js
+++ src/app/components/bandera.js
@@ -2,7 +2,11 @@
 
 import axios from 'axios';
 export async function Bandera() {
-    const response = await axios.get("https://countriesnow.space/api/v0.1/countries/flag/images")
-    console.log(response)
+    const randomId = Math.floor(Math.random() * 898) + 1;
+    const response = await axios.get("https://countriesnow.space/api/v0.1/countries/flag/images");
     return response.data;
 }
+/*console.log(bandera);
+      const resp = bandera;
+      document.getElementById("e").innerHTML = resp
+      */
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,22 @@
-import Image from "next/image";
+'use client';
 import styles from "./page.module.css";
-import bandera from "./components/bandera.js"
+//import Bandera from "./components/bandera.js"
+import { useEffect } from "react";
 
 export default function Home() {
 
-
-    
-    
+  useEffect(() => {
+    const response = fetch("https://countriesnow.space/api/v0.1/countries/flag/images")
+    .then((response) => response.json())// esto funciona 
+    .then((bandera) => {
+      console.log(bandera);
+      const resp = bandera.toString();
+      document.getElementById("e").innerHTML = resp 
+    })
+  })
     return (
     <main className={styles.main}>
-      
+      <p id="e">a</p>
     </main>
   );
 }
