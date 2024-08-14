45964e61a8f038c2ccde9da687232a7941d014d1
Julian <jelaverallauro@gmail.com>
2024-08-14 14:31:05 -0300

    inv

--- src/app/components/bandera.js
+++ src/app/components/bandera.js
@@ -4,11 +4,10 @@ import axios from 'axios';
 export async function conseguirBandera() {
     const randomId = Math.floor(Math.random() * 219);
     const response = await axios.get("https://countriesnow.space/api/v0.1/countries/flag/images");
-    console.log(response); // Recorrer array con el numero de arriba de alguna manera
-    response.
-   response.json.filter(element => element.id = randomId)
-    console.log(response)
-
+    //console.log(Object.values(response)); // Recorrer array con el numero de arriba de alguna manera
+    //.filter(element => element.id = randomId)
+    var data = (JSON.stringify(response))
+    data = data.find(buscar => randomId)
     return response.data;
 }
 /*console.log(bandera);
