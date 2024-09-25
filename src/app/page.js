a1847fe4301698df59129454aeb6453b09306f0a
Julian <jelaverallauro@gmail.com>
2024-09-25 09:07:43 -0300

    css

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,9 @@ import { useState, useEffect } from "react";
 import { conseguirBandera } from "./components/bandera";
 
 export default function Home() {
+  var [banderaIC3, fijarBanderaIC3] = useState(4);
+  var [banderaIC2, fijarBanderaIC2] = useState(3);
+  var [banderaIC1, fijarBanderaIC1] = useState(2);
   var [bandera, fijarBandera] = useState(1);
   const [loading, setLoading] = useState(true);
 
@@ -15,8 +18,13 @@ export default function Home() {
     //console.log(bdata)
     setLoading(false);
   }
-  function getBopcion() {
-    const bdata = conseguirBandera()
+  const getBopcion = async () => {
+    const bdata = await conseguirBandera()
+    fijarBanderaIC1(bdata)
+    const bdata2 = await conseguirBandera()
+    fijarBanderaIC2(bdata2)
+    const bdata3 = await conseguirBandera()
+    fijarBanderaIC3(bdata3)
     return bdata.name;
     //console.log(bdata)
   }
@@ -25,6 +33,7 @@ export default function Home() {
   useEffect(() => {
 
     getBandera()
+    getBopcion()
 
   }, []);
   
@@ -34,9 +43,12 @@ export default function Home() {
         <div className={styles.center}>
           <img src={bandera.flag} width="40%" height="40%"></img>
         </div>
-        <h1>Que bandera es esta?</h1>
+        <h1 className={styles.center}>Que bandera es esta?</h1>
       
-        <button type="button">{bandera.name}</button>
+        <button type="button" className={styles.button}>{bandera.name}</button>
+        <button type="button"className={styles.button}>{banderaIC1.name}</button>
+        <button type="button"className={styles.button}>{banderaIC2.name}</button>
+        <button type="button"className={styles.button}>{banderaIC3.name}</button>
       </div>
     </main>
   );
